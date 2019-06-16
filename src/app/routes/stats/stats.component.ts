60465bb7274757f1f052d468669d956495920a4d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-16 16:57:05 +0200

    stats

--- src/app/routes/stats/stats.component.ts
+++ src/app/routes/stats/stats.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
-
+  name = 'Saint-Louis';
+  total = 1234;
+  survivors = 102;
   constructor() { }
 
   ngOnInit() {
